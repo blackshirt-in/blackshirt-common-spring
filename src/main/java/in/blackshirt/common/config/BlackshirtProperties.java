877cd0d399cb0e0080e1a2bc4f54c7efbef03e97
Devashish Mondal <debashishmon87@gmail.com>
2026-07-11 21:46:38 +0530

    fix added

--- src/main/java/in/blackshirt/common/config/BlackshirtProperties.java
+++ src/main/java/in/blackshirt/common/config/BlackshirtProperties.java
@@ -10,9 +10,17 @@ public class BlackshirtProperties {
         public static class Log {
                 private String serviceName;
 
+                public void setServiceName(String serviceName) {
+                        this.serviceName = serviceName;
+                }
+
                 public String getServiceName() {
                         return serviceName;
                 }
+        }
+
+        public void setLog(Log log) {
+                this.log = log;
         }
 
         public Log getLog() {
